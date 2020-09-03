b25872f42678f98a6a4977b27fcabdeb95510817
unknown <wanvipa1906@gmail.com>
2020-09-03 10:55:09 +0700

    connect angular-flask

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http'
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { TasksComponent } from './task/tasks.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TasksComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    HttpClientModule,    
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
