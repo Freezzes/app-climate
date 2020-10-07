f87a456e783457097641c471db15403e098e9649
KornkanokSumran <mewkkn@hotmail.com>
2020-10-08 00:54:20 +0700

    plot data color

--- client/src/app/components/netcdf/lib/map.js
+++ client/src/app/components/netcdf/lib/map.js
@@ -17,6 +17,11 @@ import * as d3 from 'C:/Users/Mewkkn/Downloads/d3/d3';
 
 console.log("/map/lib/map.js work!")
 
+var colorScale = d3.scaleThreshold()
+    .domain([-20, -10, 0, 10, 20])
+    .range(['#bd1726', '#f7844e', '#fff7b3', '#a3d3e6', '#3a54a4']);
+
+
 export function draw_map(target) {
 
 //-----------------------------------polygon-----------------------------------------------
@@ -38,13 +43,48 @@ export function draw_map(target) {
       'features': [
         { 'type': 'Feature',
           'geometry': {
-            'type': 'Polygon',
-            'coordinates': [[[20, 0], [20, 2], [22, 2],
-            [22, 0], [20, 0]]]  
-          }
-        }]
+            'type': 'Point',
+            'coordinates' : [0,0]
+            // 'type': 'Polygon',
+            // 'coordinates': [[[20, 0], [20, 2], [22, 2],
+            // [22, 0], [20, 0]]]  
+          },
+          'properties': {
+            'value': -20
+          },
+        },
+        {
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [0,2.5]
+          },
+          'properties': {
+            'value': -10
+          },
+        },
+        {
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [2.5,0]
+          },
+          'properties': {
+            'value': -10
+          },
+        },
+        {
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [2.5,2.5]
+          },
+          'properties': {
+            'value': 10
+          },
+        },    
+    ]
     }
-    // console.log(geojsonObject)
   
     var polygonsource = new ol.source.Vector({
       wrapX: false,
@@ -91,7 +131,7 @@ export function draw_map(target) {
               new ol.layer.Tile({
                 source: new ol.source.OSM()
             }),
-              vectorLayer,geoLayer
+              vectorLayer//,geoLayer
               // mapvectorLayer,
             ],
             view: new ol.View({
@@ -103,20 +143,23 @@ export function draw_map(target) {
             })
           });
 
+    var datalayer = genGridData(geojsonObject);
+        map.addLayer(datalayer);
+
 //-------------------------grid-----------------------------------------------------------------------
-        const minor_gratucule = new ol.Graticule({
+    const minor_gratucule = new ol.Graticule({
           // the style to use for the lines, optional.
-          maxLines: 200,
-          intervals: [5, 5],
-          targetSize: 40,
-          strokeStyle: new ol.style.Stroke({
-            color: 'rgba(0,88,212,0.25)',
-            width: 0.5,
-          }),
-        });
-        minor_gratucule.setMap(map);
+      maxLines: 200,
+      intervals: [5, 5],
+      targetSize: 40,
+      strokeStyle: new ol.style.Stroke({
+      color: 'rgba(0,88,212,0.25)',
+      width: 0.5,
+      }),
+    });
+    minor_gratucule.setMap(map);
       
-        const major_graticule = new ol.Graticule({
+    const major_graticule = new ol.Graticule({
           maxLines: 100,
           intervals: [20, 20],
           targetSize: 40,
@@ -125,25 +168,75 @@ export function draw_map(target) {
             width: 1,
           }),
           showLabels: true
-        });
-        major_graticule.setMap(map);
+    });
+    major_graticule.setMap(map);
 
-      var colorScale = d3.scaleThreshold()
-        .domain([-20, -10, 0, 10, 20])
-        .range(['#bd1726', '#f7844e', '#fff7b3', '#a3d3e6', '#3a54a4']);
-    
-    // console.log(map)
-      return map
+    return map
       
-    }
+  }
 
-  
+  export function genGridData(geojson, gridSize=2.5) {
+    // debugger;
+    createLegend(colorScale);
+    console.log('grid')
+      //  
+    var gridStyle = function (feature) {
+      console.log('hi')
+      var coordinate = feature.getGeometry().getCoordinates(),
+        x = coordinate[0]- gridSize / 2,
+        y = coordinate[1]- gridSize / 2,
+        pop = parseInt(feature.getProperties().value),
+        rgb = d3.rgb(colorScale(pop));
+        console.log(x,y);
+        if (isNaN(pop)) { return }
+          return [
+            new ol.style.Style({
+              fill: new ol.style.Fill({
+                color: [rgb.r, rgb.g, rgb.b, 0.6]
+              }),
+              geometry: new ol.geom.Polygon([[
+               [x,y], [x, y + gridSize], [x + gridSize, y + gridSize], [x + gridSize, y], [x,y]
+              ]])
+            })
+          ];
+      };
+      
+    var grid = new ol.source.Vector({
+      features: (new ol.format.GeoJSON()).readFeatures(geojson)
+    });
+      
+    var gridLayer = new ol.layer.Vector({
+      source: grid,
+      style: gridStyle
+    });
     
-        
+    return gridLayer
+  }
 
-        
-        
-        
-        
-
- 
+  function createLegend (colorScale) {
+      var x = d3.scaleLinear()
+          .domain([-20, 20])
+          .range([10, 330]);
+  
+      var xAxis = d3.axisBottom()
+          .scale(x)
+          .tickSize(14)
+          .tickValues(colorScale.domain());
+  
+      var svg = d3.select('svg.legend');
+  
+      svg.selectAll('rect')
+          .data(colorScale.range().map(function(color) {
+              var d = colorScale.invertExtent(color);
+              if (d[0] == null) d[0] = x.domain()[0];
+              if (d[1] == null) d[1] = x.domain()[1];
+              return d;
+          }))
+          .enter().append('rect')
+          .attr('height', 10)
+          .attr("x", function(d) { return x(d[0]); })
+          .attr('width', function(d) { return x(d[1]) - x(d[0]); })
+          .style('fill', function(d) { return colorScale(d[0]); });
+  
+      svg.call(xAxis);
+  }
