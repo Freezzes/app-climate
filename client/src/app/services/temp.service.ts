e94a0a5a2abb28edf3d3c39597d80d02060820bc
KornkanokSumran <Mewkkn@hotmail.com>
2021-03-10 16:50:23 +0700

    add tempservice

--- client/src/app/services/temp.service.ts
+++ client/src/app/services/temp.service.ts
@@ -9,129 +9,18 @@ export class TempService {
     private API_URL = 'http://127.0.0.1:5500';
     constructor(private http: HttpClient) { }
 
-    async getTemp(): Promise<Temp[]> {
-        let result = [];
-        this.http.get<Temp[]>('http://127.0.0.1:5500/api/tmean')
-            .subscribe(res => {
-                result.push(res);
-            })
-        return result;
-    }
-
-    async getMean(): Promise<Plot[]> {
-        let result = [];
-        this.http.get<Plot[]>('http://127.0.0.1:5500/api/plot')
-            .subscribe(res => {
-                result.push(res);
-            })
-        return result;
-    }
-
-    async getMeanDB(): Promise<meanplot[]> {
-        let result = [];
-        this.http.get<meanplot[]>('http://127.0.0.1:5500/api/meantem')
-            .subscribe(res => {
-                result.push(res);
-            })
-        return result;
-    }
-
-    async getrain(): Promise<station5[]> {
-        let result = [];
-        this.http.get<station5[]>('http://127.0.0.1:5500/api/rain')
-            .subscribe(res => {
-                result.push(res);
-            })
-        return result;
-    }
-
-    async getstation(): Promise<st[]> {
-        let result = [];
-        this.http.get<st[]>('http://127.0.0.1:5500/api/station')
-            .subscribe(res => {
-                result.push(res);
-            })
-        return result;
-    }
-
-    async getrangeyear(station:string,startyear:string,endyear:string,startmonth:string,endmonth:string,startday:string,endday:string):Promise<Observable<any>> {   
-        return this.http.get('http://127.0.0.1:5500/api' +
-                         `/rangeyear?station=${station}&startyear=${startyear}&endyear=${endyear}&startmonth=${startmonth}&endmonth=${endmonth}&startday=${startday}&endday=${endday}`)
-                         
-    }
-
-    async getrangecsv(station:string,startyear:string,stopyear:string,startmonth:string,stopmonth:string):Promise<Observable<any>> {   
-        return this.http.get('http://127.0.0.1:5500/api' +
-                         `/rangecsv?station=${station}&startyear=${startyear}&stopyear=${stopyear}&startmonth=${startmonth}&stopmonth=${stopmonth}`)
-                         
-    }
-
-    async getboxvalue(station:string,start_date:string,end_date:string):Promise<Observable<any>>{
-        return this.http.get('http://127.0.0.1:5500/api'+
-                        `/boxplotvalue?station=${station}&start_date=${start_date}&end_date=${end_date}`)
-    }
-
-    async getMissed(): Promise<Missing[]> {
-        let result = [];
-        this.http.get<Missing[]>('http://127.0.0.1:5500/api/missing')
-            .subscribe(res => {
-                result.push(res);
-            })
-        return result;
-    }
-    async getmissing(sta:string,startyear:string,stopyear:string,dff:string):Promise<Observable<any>>{
-        return this.http.get('http://127.0.0.1:5500/api'+
-                        `/selectmissing?sta=${sta}&startyear=${startyear}&stopyear=${stopyear}&dff=${dff}`
-        )}
-
-    getData(){
-        return this.http.get('http://127.0.0.1:5500/api/missing')
-    }
-
-    async get_Avgcsv(ncfile:string,df_f:string,startyear:string,stopyear:string,startmonth:string,stopmonth:string):Promise<Observable<any>> {
-        return this.http.get('http://127.0.0.1:5500' +
-        `/nc_avg?ncfile=${ncfile}&df_f=${df_f}&startyear=${startyear}&stopyear=${stopyear}&startmonth=${startmonth}&stopmonth=${stopmonth}`);
-    }
-
-    async nc_defer(df_f:string,start1:string,stop1:string,start2:string,stop2:string):Promise<Observable<any>> {
-        return this.http.get('http://127.0.0.1:5500' +
-        `/nc_defer?df_f=${df_f}&start1=${start1}&stop1=${stop1}&start2=${start2}&stop2=${stop2}`);
-    }
-
-    async nc_per(df_f:string,start1:string,stop1:string,start2:string,stop2:string):Promise<Observable<any>> {
-        return this.http.get('http://127.0.0.1:5500' +
-        `/nc_per?df_f=${df_f}&start1=${start1}&stop1=${stop1}&start2=${start2}&stop2=${stop2}`);
-    }
-
-    getdata_test(): Observable<data_test[]>{
-        console.log("service",this.http.get<data_test[]>('http://127.0.0.1:5500/api/getdata'))
-        return this.http.get<data_test[]>('http://127.0.0.1:5500/api/getdata')
-
-    }
-
     async getdata_sta(df_f:string,startyear:string,stopyear:string,startmonth:string,stopmonth:string): Promise<Observable<any>>{
         // console.log("service",this.http.get('http://127.0.0.1:5500/locat/station'))
         return this.http.get('http://127.0.0.1:5500'+ `/locat/station?df_f=${df_f}&startyear=${startyear}&stopyear=${stopyear}&startmonth=${startmonth}&stopmonth=${stopmonth}`)
     }
 
-    // async get_grid_db(dataset:string): Promise<Observable<any>>{
-    //     // console.log("service",this.http.get('http://127.0.0.1:5500/locat/station'))
-    //     return this.http.get('http://127.0.0.1:5500'+ `/api/get_grid?dataset=${dataset}`)
-    // }
-
-    
-    async get_grid_db(dataset:string): Promise<Observable<grid[]>>{
-        console.log("service",this.http.get<grid[]>('http://127.0.0.1:5500'+ `/api/get_grid?dataset=${dataset}`))
-        return this.http.get<grid[]>('http://127.0.0.1:5500'+ `/api/get_grid?dataset=${dataset}`)
-    }
-
-    async get_data_db(dataset:string,index:string,startyear:string,startmonth:string): Promise<Observable<any>>{
+    async get_grid_db(dataset:string): Promise<Observable<any>>{
         // console.log("service",this.http.get('http://127.0.0.1:5500/locat/station'))
-        return this.http.get('http://127.0.0.1:5500/' +`api/get_db?dataset=${dataset}&index=${index}&startyear=${startyear}&startmonth=${startmonth}`)
+        return this.http.get('http://127.0.0.1:5500'+ `/api/get_grid?dataset=${dataset}`)
     }
 
     getdata_db(dataset:string,index:string,startyear:string,startmonth:string){
-        const response = this.http.get('http://127.0.0.1:5500/api/get_db?dataset=TMD&index=pre&startyear=1903&startmonth=2',{responseType:"text"})
+        const response = this.http.get('http://127.0.0.1:5500/'+`api/get_db?dataset=${dataset}&index=${index}&startyear=${startyear}&startmonth=${startmonth}`,{responseType:"text"})
         
         // console.log("service_data", response)
         return response
@@ -141,5 +30,52 @@ export class TempService {
         //         return await JSON.parse(JSON.stringify(res)); 
         //     }
         // )
+    }
+
+    async get_detail(dataset:string,index:string){
+        const response = this.http.get('http://127.0.0.1:5500/'+`api/detail_index?dataset=${dataset}&index=${index}`,{responseType:"text"})
+        // console.log("service_detail", response)
+        return response
+    }
+
+    async get_data_flask(dataset:string,index:string,startyear:string,startmonth:string,stopyear:string,stopmonth:string){
+        const response = this.http.get('http://127.0.0.1:5500/'+
+            `/api/data_avg?dataset=${dataset}&index=${index}&startyear=${startyear}&startmonth=${startmonth}&stopyear=${stopyear}&stopmonth=${stopmonth}`
+            ,{responseType:"text"}
+            )
+        return response
+    }
+
+    async data_dif(dataset:string,index:string,start1:string,stop1:string,start2:string,stop2:string):Promise<Observable<any>> {
+        return this.http.get('http://127.0.0.1:5500' +
+        `/api/data_dif?dataset=${dataset}&index=${index}&start1=${start1}&stop1=${stop1}&start2=${start2}&stop2=${stop2}`);
+    }
+
+    async get_Avgcsv(ncfile:string,df_f:string,startyear:string,stopyear:string,startmonth:string,stopmonth:string){
+        const response = this.http.get('http://127.0.0.1:5500' +
+        `/nc_avg?ncfile=${ncfile}&df_f=${df_f}&startyear=${startyear}&stopyear=${stopyear}&startmonth=${startmonth}&stopmonth=${stopmonth}`
+        ,{responseType:"text"});
+
+        return response
+        }
+
+    async get_hire(ncfile:string,df_f:string,startyear:string,stopyear:string,startmonth:string,stopmonth:string){
+        const response = this.http.get('http://127.0.0.1:5500' +
+        `/nc_avg_hire?ncfile=${ncfile}&df_f=${df_f}&startyear=${startyear}&stopyear=${stopyear}&startmonth=${startmonth}&stopmonth=${stopmonth}`
+        ,{responseType:"text"});
+
+        return response
+        }
+
+    async nc_per(ncfile:string,df_f:string,start1:string,stop1:string,start2:string,stop2:string):Promise<Observable<any>> {
+        return this.http.get('http://127.0.0.1:5500' +
+        `/nc_per?ncfile=${ncfile}&df_f=${df_f}&start1=${start1}&stop1=${stop1}&start2=${start2}&stop2=${stop2}`
+        ,{responseType:"text"});
+    }
+
+    async global_avg(dataset:string,index:string,startyear:string,startmonth:string,stopyear:string,stopmonth:string){
+        const response = this.http.get('http://127.0.0.1:5500' + 
+        `/api/global_avg?dataset=${dataset}&index=${index}&startyear=${startyear}&startmonth=${startmonth}&stopyear=${stopyear}&stopmonth=${stopmonth}`)
+        return response
     }
 }
